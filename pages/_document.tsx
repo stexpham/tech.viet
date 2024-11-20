11f6d17d2fafd8bb41b6d007bacf014ac85f01f6
Ste X. Pham <ste@stepham.com>
2024-11-20 12:19:36 +0700

    fix

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,6 +1,5 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document';
-import { Adsense } from '
-../components/adsense/AutoadsScript';
+import { Adsense } from '../components/adsense/AutoadsScript';
 import { GA_TRACKING_ID } from '../lib/gtag';
 export default class MyDocument extends Document {
   render() {
